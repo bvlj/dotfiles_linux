c01f8a0a81896073e645f51e88a77e4e9017b8c3
Joey <bevilacquajoey@gmail.com>
2023-09-03 22:06:57 +0200

    Add mac-os like copy-paste command aliases

--- .config/shell/env.sh
+++ .config/shell/env.sh
@@ -19,6 +19,10 @@ alias ls='ls -1'
 alias open='xdg-open'
 # rand-str
 alias rand-str="LC_ALL=C tr -dc 'A-Za-z0-9!#$%&~' < /dev/urandom | head -c $1"
+# clipboard (macos-like)
+alias pbcopy="xclip -selection c"
+alias pbpaste="xclip -selection c -o"
+
 
 ###
 # Session variables
